06eefbfe4d4be849584db2848eda17d112d25a0f
lizhongwei <lizhongwei@qutoutiao.net>
2020-09-16 20:05:20 +0800

    sync

--- Algorithms/0079.word-search/word-search.go
+++ Algorithms/0079.word-search/word-search.go
@@ -1,52 +1,91 @@
 package problem0079
+//
+//func exist(board [][]byte, word string) bool {
+//	m := len(board)
+//	if m == 0 {
+//		return false
+//	}
+//
+//	n := len(board[0])
+//	if n == 0 {
+//		return false
+//	}
+//
+//	if len(word) == 0 {
+//		return false
+//	}
+//
+//	var dfs func(int, int, int) bool
+//	dfs = func(r, c, index int) bool {
+//		if len(word) == index {
+//			return true
+//		}
+//
+//		if r < 0 || m <= r || c < 0 || n <= c || board[r][c] != word[index] {
+//			return false
+//		}
+//
+//		temp := board[r][c]
+//		board[r][c] = 0
+//
+//		if dfs(r-1, c, index+1) ||
+//			dfs(r+1, c, index+1) ||
+//			dfs(r, c-1, index+1) ||
+//			dfs(r, c+1, index+1) {
+//			return true
+//		}
+//
+//		board[r][c] = temp
+//
+//		return false
+//	}
+//
+//	for i := 0; i < m; i++ {
+//		for j := 0; j < n; j++ {
+//			if dfs(i, j, 0) {
+//				return true
+//			}
+//		}
+//	}
+//
+//	return false
+//}
+
 
 func exist(board [][]byte, word string) bool {
-	m := len(board)
-	if m == 0 {
+	row := len(board)
+	if row == 0{
 		return false
 	}
-
-	n := len(board[0])
-	if n == 0 {
+	column := len(board[0])
+	if column == 0{
 		return false
 	}
-
-	if len(word) == 0 {
-		return false
-	}
-
-	var dfs func(int, int, int) bool
-	dfs = func(r, c, index int) bool {
-		if len(word) == index {
-			return true
-		}
-
-		if r < 0 || m <= r || c < 0 || n <= c || board[r][c] != word[index] {
-			return false
-		}
-
-		temp := board[r][c]
-		board[r][c] = 0
-
-		if dfs(r-1, c, index+1) ||
-			dfs(r+1, c, index+1) ||
-			dfs(r, c-1, index+1) ||
-			dfs(r, c+1, index+1) {
-			return true
-		}
-
-		board[r][c] = temp
-
-		return false
-	}
-
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			if dfs(i, j, 0) {
+	for i:=0;i< len(board);i++{
+		for j:=0;j < column;j++{
+			exist := dfs(board,i,j,word,0)
+			if exist {
 				return true
 			}
 		}
 	}
-
 	return false
+}
+
+func dfs(board[][]byte, i,j int,word string, index int) bool{
+	if len(word) == index{
+		return true
+	}
+	if i < 0 || j < 0 || i >= len(board) || j >= len(board[0]) {
+		return false
+	}
+	exist := false
+	if board[i][j] == word[index]{
+		board[i][j] = 0
+		if dfs(board,i-1,j, word,index+1) ||  dfs(board,i,j-1, word,index+1) ||  dfs(board,i+1,j, word,index+1) ||  dfs(board,i,j+1, word,index+1){
+			exist = true
+		}
+		board[i][j] = word[index]
+	}
+	return exist
 }
--- Algorithms/0208.implement-trie-prefix-tree/implement-trie-prefix-tree.go
+++ Algorithms/0208.implement-trie-prefix-tree/implement-trie-prefix-tree.go
@@ -9,59 +9,111 @@ package problem0208
  */
 
 // Trie 是便于 word 插入与查找的数据结构
+//type Trie struct {
+//	val  byte
+//	sons [26]*Trie
+//	end  int
+//}
+//
+//func Constructor() Trie {
+//	return Trie{}
+//}
+//
+//func (this *Trie) Insert(word string) {
+//	node := this
+//	size := len(word)
+//	for i := 0; i < size; i++ {
+//		idx := word[i] - 'a'
+//		if node.sons[idx] == nil {
+//			node.sons[idx] = &Trie{val: word[i]}
+//		}
+//
+//		node = node.sons[idx]
+//	}
+//
+//	node.end++
+//}
+//
+//func (this *Trie) Search(word string) bool {
+//	node := this
+//	size := len(word)
+//	for i := 0; i < size; i++ {
+//		idx := word[i] - 'a'
+//		if node.sons[idx] == nil {
+//			return false
+//		}
+//		node = node.sons[idx]
+//	}
+//
+//	if node.end > 0 {
+//		return true
+//	}
+//
+//	return false
+//}
+//
+//func (this *Trie) StartsWith(prefix string) bool {
+//	node := this
+//	size := len(prefix)
+//	for i := 0; i < size; i++ {
+//		idx := prefix[i] - 'a'
+//		if node.sons[idx] == nil {
+//			return false
+//		}
+//		node = node.sons[idx]
+//	}
+//
+//	return true
+//}
+
+
 type Trie struct {
-	val  byte
-	sons [26]*Trie
-	end  int
+	isWord   bool
+	children map[rune]*Trie
 }
 
-func Constructor() Trie {
-	return Trie{}
+/** Initialize your data structure here. */
+func Constructor208() Trie {
+	return Trie{isWord: false, children: make(map[rune]*Trie)}
 }
 
+/** Inserts a word into the trie. */
 func (this *Trie) Insert(word string) {
-	node := this
-	size := len(word)
-	for i := 0; i < size; i++ {
-		idx := word[i] - 'a'
-		if node.sons[idx] == nil {
-			node.sons[idx] = &Trie{val: word[i]}
+	parent := this
+	for _, ch := range word {
+		if child, ok := parent.children[ch]; ok {
+			parent = child
+		} else {
+			newChild := &Trie{children: make(map[rune]*Trie)}
+			parent.children[ch] = newChild
+			parent = newChild
 		}
-
-		node = node.sons[idx]
 	}
-
-	node.end++
+	parent.isWord = true
 }
 
+/** Returns if the word is in the trie. */
 func (this *Trie) Search(word string) bool {
-	node := this
-	size := len(word)
-	for i := 0; i < size; i++ {
-		idx := word[i] - 'a'
-		if node.sons[idx] == nil {
-			return false
+	parent := this
+	for _, ch := range word {
+		if child, ok := parent.children[ch]; ok {
+			parent = child
+			continue
 		}
-		node = node.sons[idx]
+		return false
 	}
-
-	if node.end > 0 {
-		return true
-	}
-
-	return false
+	return parent.isWord
 }
 
+/** Returns if there is any word in the trie that starts with the given prefix. */
 func (this *Trie) StartsWith(prefix string) bool {
-	node := this
-	size := len(prefix)
-	for i := 0; i < size; i++ {
-		idx := prefix[i] - 'a'
-		if node.sons[idx] == nil {
-			return false
+	parent := this
+	for _, ch := range prefix {
+		if child, ok := parent.children[ch]; ok {
+			parent = child
+			continue
 		}
-		node = node.sons[idx]
+		return false
 	}
-
 	return true
 }
